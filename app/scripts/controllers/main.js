431b20487e7b0e3afa57644897d383b9cc5de541
Jeremy Lautman <Jeremy Lautman>
2016-09-09 22:09:57 -0400

    Commit generated scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackTeam5UiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hackTeam5UiApp
+ */
+angular.module('hackTeam5UiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
