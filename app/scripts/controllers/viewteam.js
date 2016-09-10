abab02c36dbe4524395689dfeaea335350aa77b0
judykong97 <junhank@andrew.cmu.edu>
2016-09-10 17:40:43 -0400

    Basic Functionalities of View Team

--- app/scripts/controllers/viewteam.js
+++ app/scripts/controllers/viewteam.js
@@ -7,66 +7,15 @@
  * # ViewteamCtrl
  * Controller of the hackTeam5UiApp
  */
-angular.module('hackTeam5UiApp')
-  .controller('ViewteamCtrl', function ($scope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-  $scope.oneAtATime = true;
-
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
-
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-  };
-
-  $scope.status = {
-    isCustomHeaderOpen: false,
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
-});
-
-angular.module('hackTeam5UiApp').controller('AccordionDemoCtrl', function ($scope) {
-  $scope.oneAtATime = true;
-
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
-
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-  };
-
-  $scope.status = {
-    isCustomHeaderOpen: false,
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
-});
-
+angular.module('hackTeam5UiApp')  
+.controller('ViewteamCtrl', ['$scope', 'viewTeam', function ($scope, viewTeam) {
+    viewTeam.getTeam(1).then(
+      function (data) {
+        $scope.teamdata = data;
+        console.log($scope.teamdata);
+      },
+      function (error) {
+        //TODO
+      }
+    );
+  }]);
--- app/scripts/services/viewteam.js
+++ app/scripts/services/viewteam.js
@@ -8,32 +8,23 @@
  * Service in the hackTeam5UiApp.
  */
 angular.module('hackTeam5UiApp')
-  .service('viewTeam', function ($scope) {
-
-  $scope.oneAtATime = true;
-
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
+  .service('viewTeam', function ($q) {
+    this.getTeam = function (id) {
+      return $q.when({
+      id: 1,
+      pic_id: 1,
+      team_name: "fake hackers",
+      spots_available: 3,
+      blurb: "This is a fake team. Don't join us.",
+      theme_of_hack: "brainstorming",
+      current_skill_sets: ["javascript", "android studio"],
+      // existing_members is a list of member ids
+      existing_members: [1],
+      // location links to the location of the team (Not sure how this should be presented yet)
+      location: "ajsdfasdfasdf",
+      has_a_guru: false,
+      looking_for_skills: ["UI", "leadership(?)"]
+      });
     }
-  ];
-
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-  };
-
-  $scope.status = {
-    isCustomHeaderOpen: false,
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
     // AngularJS will instantiate a singleton by calling "new" on this function
   });
