dfc61a6e45a03d5db1e3c86b574d2a65478425c7
judykong97 <junhank@andrew.cmu.edu>
2016-09-11 02:21:53 -0400

    Final merge

--- app/scripts/services/teamservice.js
+++ app/scripts/services/teamservice.js
@@ -12,19 +12,19 @@ angular.module('hackTeam5UiApp')
      this.getTeam = function (id) {
          console.log('Asked for team id', id);
          return $q.when({
-             id: 1,
-            pic_id: 1,
-            team_name: "fake hackers",
-            spots_available: 3,
-            blurb: "This is a fake team. Don't join us.",
-            theme_of_hack: "brainstorming",
-            current_skill_sets: ["javascript", "android studio"],
-            // existing_members is a list of member ids
-            existing_members: [1],
-            // location links to the location of the team (Not sure how this should be presented yet)
-            location: "ajsdfasdfasdf",
-            has_a_guru: "Yes",
-            looking_for_skills: ["UI", "leadership(?)"]
+  			id: 1,
+			pic_id: 1,
+			team_name: "Fake Hackers",
+			spots_available: 2,
+			blurb: "This is a fake team. Don't join us.",
+			theme_of_hack: "Brainstorming",
+			current_skill_sets: ["Javascript", "Android Studio"],
+			// existing_members is a list of member ids
+			existing_members: [2],
+			// location links to the location of the team (Not sure how this should be presented yet)
+			location: "Meet at Levine, 5 pm",
+			has_a_guru: "Yes",
+			looking_for_skills: ["UI", "good ideas"]
          });
      };
    // AngularJS will instantiate a singleton by calling "new" on this function
